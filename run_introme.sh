66658e0345f63c527242a402410971cee2952d07
SarahBeecroft <sarah.beecroft@uwa.edu.au>
2018-05-17 15:16:41 +0800

    Update run_introme.sh

--- run_introme.sh
+++ run_introme.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+#usage is ./introme.sh input.joint.vcf family_ID inheritance_pattern
 #input joint VCF
 input_VCF=$1
 #used R_170503_GINRAV_DNA_M001.hc.vqsr.vep.vcf.gz
 #Family structure
-#define affected samples
+Family=$2
+inheritance_pattern=$3
 PROBAND=D16-1922
-AFF2=D16-0862
-AFF3=D03-114
-#define UNaffected samples
-#HEALTHY1=
-#HEALTHY2=
+MUM=D16-0862
+DAD=D03-114
+GRANNY=xxxxx
+affected=$(echo %$PROBAND %$MUM %$DAD)
+unaffected=$(echo %GRANNY)
 #directories
-working_dir=/data/intronome/outputs
+working_dir=/data/intronome
+out_dir/data/intronome/outputs
 ref_dir=/data/intronome/references
 data_dir=/data/introme/data
 #genome subset files
@@ -20,18 +23,14 @@ intron=UCSC_intron.bed
 3UTR=UCSC_3primeUTR.bed
 2000US=references/UCSC_2000_upstream.bed
 MotifFeatInput=homo_sapiens.GRCh37.motiffeatures.20161117.gff.gz
-MotifFeat=Ens_MotifFeat
 RegFeatInput=homo_sapiens.GRCh37.Regulatory_Build.regulatory_features.20161117.gff.gz 
-RegFeat=Ens_RefFeat
-#Patient selection
-Family=F1
 #Hard filter cutoffs for step 3
-gnomad_AF= <0.5
-gnomad_AC= <=100
-DANN_score= >=0.8
-MGRB_AC= <=50
-fathmm-MKL= >=0.6
-CADD_phred= >=10
+gnomad_AF='<0.5'
+gnomad_AC='<=100'
+DANN_score='>=0.8'
+MGRB_AC='<=50'
+fathmm-MKL='>=0.6'
+CADD_phred='>=10
 
 ###STEP ONE- subsetting your joint VCF to genomic regions of interest
 echo $(date +%x_%r) 'beginning intersection'
@@ -49,43 +48,35 @@ echo $(date +%x_%r) 'intersection complete'
 echo $(date +%x_%r) 'annotation beginning'
 vcfanno $ref_dir/conf.toml $out_dir/$Fam.subset.vcf.gz | \
 bgzip > $out_dir/$Fam.subset.annotated.vcf.gz
-&& tabix -p vcf $out_dir/$Fam.subset.annotated.vcf.gz
+tabix -p vcf $out_dir/$Fam.subset.annotated.vcf.gz
 echo $(date +%x_%r) 'annotation complete'
 zcat $out_dir/annotated.$Fam.subset.vcf.gz | grep '#' | gzip > $out_dir/$Fam.subset.annotated.vcf.header.gz
 
 #Step 3- Hard filtering your subsetted, annotated VCF
 #Filtering thresholds will depend on your application. this is a guide. A useful doc is http://www.enlis.com/blog/2015/03/17/the-best-variant-prediction-method-that-no-one-is-using/
 echo $(date +%x_%r) 'filtering' $out_dir/$Fam.subset.annotated.vcf.gz
+bcftools filter -i'gn_af$gnomad_AF || gn_ac$gnomad_AC || DANN_score$DANN_score || mgrb_ac$MGRB_AC || fathmm-MKL_non-coding$fathmm-MKL || CADD_phred$CADD_phred' $out_dir/$Fam.subset.annotated.vcf.gz | bgzip > $out_dir/$Fam.subset.tmp.vcf.gz
 
-zcat $out_dir/$Fam.subset.annotated.vcf.gz | perl -ne 'if ($_ =~ /gn_af=(.*?);/) { if ($1 $gnomad_AF) { print $_; }}' | \
-perl -ne 'if ($_ =~ /gn_ac=(.*?);/) { if ($1 $gnomad_AC) { print $_; }}' | \
-perl -ne 'if ($_ =~ /DANN_score=(.*?);/) { if ($1 $DANN_score) { print $_; }}' | \
-perl -ne 'if ($_ =~ /mgrb_ac=(.*?);/) { if ($1 $MGRB_AC) { print $_; }}' | \
-perl -ne 'if ($_ =~ /fathmm-MKL_non-coding=(.*?);/) { if ($1 $fathmm-MKL) { print $_; }}' | \
-perl -ne 'if ($_ =~ /CADD_phred=(.*?);/) { if ($1 $CADD_phred) { print $_; }}' | \
-bgzip > $out_dir/$Fam.subset.tmp.vcf.gz
 #add header information back again
 zcat $out_dir/$Fam.subset.annotated.vcf.header.gz zcat $out_dir/$Fam.subset.tmp.vcf.gz | \
-bgzip > $out_dir/$Fam.subset.annotated.filteredvcf.gz
-tabix $out_dir/$Fam.subset.annotated.filteredvcf.gz
-echo $(date +%x_%r) 'completed filtering' $annotatedVCF
+bgzip > $out_dir/$Fam.subset.annotated.filtered.vcf.gz
+tabix $out_dir/$Fam.subset.annotated.filtered.vcf.gz
+rm $out_dir/$Fam.subset.tmp.vcf.gz
+echo $(date +%x_%r) 'completed filtering' 
 
 ##STEP 4 - family filtering
 #de novo filter 
-PROBAND=D16-1922
-AFF2=D16-0862
-AFF3=D03-114
-
-Dominant
-bcftools filter -i'0/1' -f'%$PROBAND %$AFF2 %$AFF3\n' $file > dominant.gt_filter.%file
-
-De novo
-bcftools filter -i'0/1' -f'%$PROBAND\n' || -e'0/1' -f'%$MUM %$DAD\n' $file > denovo.gt_filter.%file
-
-Homo recessive
-bcftools filter -i'1/1' -f'%$PROBAND %$AFF2 %$AFF3\n' || -i'0/1' -f '%$MUM %$DAD\n' $file > homRec.gt_filter.%file
-if filter=de_novo
-then
-do
-
-print where GT 0/1
+if [ "$inheritance_pattern" = "de novo"]; then
+  echo "applying de novo genotype filter"
+  bcftools filter -i'0/1' -f'%$PROBAND\n' || -e'0/1' -f'%$MUM %$DAD\n' || -e'1/1' -f'%$MUM %DAD/n' $out_dir/$Fam.subset.annotated.filtered.vcf.gz | bgzip > $out_dir/$Fam.subset.annotated.filtered.de_novo.vcf.gz && tabix $out_dir/$Fam.subset.annotated.filtered.de_novo.vcf.gz
+  elif ["$inheritance_pattern" = "homozygous recessive"]; then
+  echo "applying homozygous recessive genotype filter"
+  bcftools filter -i'1/1' -f'%$PROBAND\n' || -i'0/1' -f '%$MUM %$DAD\n' out_dir/$Fam.subset.annotated.filtered.vcf.gz | bgzip > $out_dir/$Fam.subset.annotated.filtered.hom_rec.vcf.gz && tabix $out_dir/$Fam.subset.annotated.filtered.hom_rec.vcf.gz
+  elif [ "$inheritance_pattern" = "dominant"]; then
+  echo "applying dominant genotype filter"
+bcftools filter -i'0/1' -f'$affected\n' || -e'1/1' -f'$affected\n' || -e'1/1' -f'$unaffected\n' || -e'0/1' -f'$unaffected\n'  $out_dir/$Fam.subset.annotated.filtered.vcf.gz | bgzip > $out_dir/$Fam.subset.annotated.filtered.dominant.vcf.gz && tabix $out_dir/$Fam.subset.annotated.filtered.dominant.vcf.gz
+ # elif [ "$inheritance_pattern" = "compound heterozygous"]; then
+  # echo "applying compound heterozygous genotype filter"
+  # bcftools filter -i'0/1' -f'%$PROBAND %$MUM\n' || -e'1/1' -f'%$PROBAND %$MUM\n' || -e'0/1' -f'%$DAD\n' out_dir/$Fam.subset.annotated.filtered.vcf.gz > $out_dir/$MUM.variants.txt
+  # bcftools filter -i'0/1' -f'%$PROBAND %$DAD\n' || -e'1/1' -f'%$PROBAND %$DAD\n' || -e'0/1' -f'%$MUM\n' out_dir/$Fam.subset.annotated.filtered.vcf.gz > $out_dir/$DAD.variants.txt
+fi
