082242592111479c65e3a659db80d1da49854285
Vel <vel@vel.nz>
2018-05-29 14:09:38 +1000

    Adding CADD annotation, rearranging code for efficiency and adding filters.

--- run_introme.sh
+++ run_introme.sh
@@ -3,12 +3,9 @@
 # Introme Bash Script
 #
 # Developers:
-# Sarah Beecroft (sarah.beecroft@uwa.edu.au)
 # Velimir Gayevskiy (vel@vel.nz)
-#
-# TODO:
-# * Need to let through variants that don't have an annotation rather than excluding them
-#
+# Sarah Beecroft (sarah.beecroft@uwa.edu.au)
+
 
 
 
@@ -42,13 +39,12 @@ prefix=$3
 # Filtering thresholds will depend on your application, we urge you to carefully consider these 
 MGRB_AF='<=0.01'
 gnomad_AF='<=0.01'
-
-#DANN_score='>=0.8'
-#fathmm_MKL='>=0.6'
-#CADD_phred='>=10'
+CADD_phred='>=10'
+min_QUAL='>=200' # The QUAL VCF field
+max_DP='>=10' # The sample with the highest depth in the VCF must have a higher depth than this value
 
 ##################################
-# STEP ONE: subsetting the VCF to genomic regions of interest
+# STEP 1: subsetting the VCF to genomic regions of interest (first because it gets rid of the most variants)
 
 echo $(date +%x_%r) 'Beginning subsetting'
 echo $(gzip -d -c $input_VCF | wc -l) 'VCF lines prior to subsetting'
@@ -60,47 +56,15 @@ echo $(gzip -d -c $out_dir/$prefix.subset.vcf.gz | wc -l) 'VCF lines after subse
 echo $(date +%x_%r) 'Subsetting complete'
 
 ##################################
-# STEP TWO: annotate the subsetted VCF with useful information, to be used for filtering downstream
+# STEP 2: familial filtering (second because it gets rid of the second most variants)
 
-echo $(date +%x_%r) 'Beginning annotation'
-
-vcfanno -p 4 conf.toml $out_dir/$prefix.subset.vcf.gz | bgzip > $out_dir/$prefix.subset.annotated.vcf.gz # The conf.toml file specifies what VCFanno should annotate
-tabix -p vcf $out_dir/$prefix.subset.annotated.vcf.gz
-
-echo $(date +%x_%r) 'Annotation complete'
-
-#zcat $out_dir/annotated.$Fam.subset.vcf.gz | grep '#' | gzip > $out_dir/$Fam.subset.annotated.vcf.header.gz
-
-##################################
-# STEP THREE: Hard filtering the subsetted, annotated VCF
-
-echo $(date +%x_%r) 'Beginning filtering'
-
-bcftools filter -i"mgrb_af$MGRB_AF && gn_af$gnomad_AF" $out_dir/$prefix.subset.annotated.vcf.gz | bgzip > $out_dir/$prefix.subset.annotated.filtered.vcf.gz
-
-# TODO which gnomad column is best to use?
-
-#bcftools filter -i'gn_af$gnomad_AF || gn_ac$gnomad_AC || DANN_score$DANN_score || mgrb_ac$MGRB_AC || fathmm-MKL_non-coding$fathmm-MKL || CADD_phred$CADD_phred' $out_dir/$Fam.subset.annotated.vcf.gz | bgzip > $out_dir/$Fam.subset.tmp.vcf.gz
-
-# Add header information back again
-#zcat $out_dir/$Fam.subset.annotated.vcf.header.gz zcat $out_dir/$Fam.subset.tmp.vcf.gz | \
-#bgzip > $out_dir/$Fam.subset.annotated.filtered.vcf.gz
-#tabix $out_dir/$Fam.subset.annotated.filtered.vcf.gz
-#rm $out_dir/$Fam.subset.tmp.vcf.gz
-
-echo $(gzip -d -c $out_dir/$prefix.subset.annotated.filtered.vcf.gz | wc -l) 'VCF lines after filtering'
-
-echo $(date +%x_%r) 'Filtering complete' 
-
-##################################
-# STEP 4: family filtering
-
-# TODO apply de novo inheritance pattern and see how many are left!
-# Read man page for bcftools for how best to do this
+echo $(date +%x_%r) 'Beginning familial filtering'
 
 #if [ "$inheritance_pattern" = "denovo"]; then
 #	echo "applying de novo genotype filter"
-#	bcftools filter -i'0/1' -f'%$PROBAND\n' || -e'0/1' -f'%$MUM %$DAD\n' || -e'1/1' -f'%$MUM %DAD/n' $out_dir/$Fam.subset.annotated.filtered.vcf.gz | bgzip > $out_dir/$Fam.subset.annotated.filtered.de_novo.vcf.gz && tabix $out_dir/$Fam.subset.annotated.filtered.de_novo.vcf.gz
+	bcftools filter --threads 8 -i"FORMAT/GT[0]='0/0' && FORMAT/GT[1]='0/0' && FORMAT/GT[2]='0/1'" $out_dir/$prefix.subset.vcf.gz | bgzip > $out_dir/$prefix.subset.denovo.vcf.gz
+	tabix $out_dir/$prefix.subset.denovo.vcf.gz
+	echo $(gzip -d -c $out_dir/$prefix.subset.denovo.vcf.gz | wc -l) 'VCF lines after de novo filter'
 #elif ["$inheritance_pattern" = "recessive"]; then
 #	echo "applying homozygous recessive genotype filter"
 #	bcftools filter -i'1/1' -f'%$PROBAND\n' || -i'0/1' -f '%$MUM %$DAD\n' out_dir/$Fam.subset.annotated.filtered.vcf.gz | bgzip > $out_dir/$Fam.subset.annotated.filtered.hom_rec.vcf.gz && tabix $out_dir/$Fam.subset.annotated.filtered.hom_rec.vcf.gz
@@ -112,6 +76,34 @@ echo $(date +%x_%r) 'Filtering complete'
 	# bcftools filter -i'0/1' -f'%$PROBAND %$MUM\n' || -e'1/1' -f'%$PROBAND %$MUM\n' || -e'0/1' -f'%$DAD\n' out_dir/$Fam.subset.annotated.filtered.vcf.gz > $out_dir/$MUM.variants.txt
 	# bcftools filter -i'0/1' -f'%$PROBAND %$DAD\n' || -e'1/1' -f'%$PROBAND %$DAD\n' || -e'0/1' -f'%$MUM\n' out_dir/$Fam.subset.annotated.filtered.vcf.gz > $out_dir/$DAD.variants.txt
 #fi
+
+echo $(date +%x_%r) 'Familial filtering complete'
+
+##################################
+# STEP 3: annotate the subsetted VCF with useful information, to be used for filtering downstream
+
+echo $(date +%x_%r) 'Beginning annotation'
+
+vcfanno -p 8 conf.toml $out_dir/$prefix.subset.denovo.vcf.gz | bgzip > $out_dir/$prefix.subset.denovo.annotated.vcf.gz # The conf.toml file specifies what VCFanno should annotate
+tabix -p vcf $out_dir/$prefix.subset.denovo.annotated.vcf.gz
+
+echo $(date +%x_%r) 'Annotation complete'
+
+##################################
+# STEP 4: Hard filtering the subsetted, annotated VCF
+
+echo $(date +%x_%r) 'Beginning filtering'
+
+bcftools filter --threads 8 -i"FILTER='PASS' && TYPE='snp' && QUAL$min_QUAL && MAX(DP)$max_DP && (mgrb_af$MGRB_AF || mgrb_af='.') && (gn_pm_af$gnomad_AF || gn_pm_af='.') && (cadd_phred$CADD_phred || cadd_phred='.')" $out_dir/$prefix.subset.denovo.annotated.vcf.gz | bgzip > $out_dir/$prefix.subset.denovo.annotated.filtered.vcf.gz
+tabix -p vcf $out_dir/$prefix.subset.denovo.annotated.filtered.vcf.gz
+
+#bcftools filter -i'gn_af$gnomad_AF || gn_ac$gnomad_AC || DANN_score$DANN_score || mgrb_ac$MGRB_AC || fathmm-MKL_non-coding$fathmm-MKL || CADD_phred$CADD_phred' $out_dir/$Fam.subset.annotated.vcf.gz | bgzip > $out_dir/$Fam.subset.tmp.vcf.gz
+
+echo $(gzip -d -c $out_dir/$prefix.subset.denovo.annotated.filtered.vcf.gz | wc -l) 'VCF lines after filtering'
+
+echo $(date +%x_%r) 'Filtering complete' 
+
+##################################
 
 # For MaxEntScan:
 # echo "ctctactactatctatctagatc" | perl score3.pl - | cut -f 2
